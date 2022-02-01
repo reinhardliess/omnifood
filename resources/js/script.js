bba31f5060c0ebb0868fab231109caad6c25dbd0
Reinhard Liess <reinhard.liess@gmail.com>
2022-02-01 17:48:29 +0100

    feat: add smooth scrolling

--- resources/js/script.js
+++ resources/js/script.js
@@ -15,5 +15,38 @@ function enableMobileNavigation() {
     bodyEl.classList.toggle("mobile-nav-open");
   });
 }
+
+// Implement smooth scrolling for all local links
+function enableSmoothScrolling() {
+  const allLinks = document.querySelectorAll("a:link");
+  const bodyEl = document.querySelector("body");
+
+  allLinks.forEach(function (element) {
+    element.addEventListener("click", function (e) {
+      const el = e.currentTarget;
+      const href = el.getAttribute("href");
+
+      e.preventDefault();
+
+      if (href === "#") {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      }
+
+      if (href.startsWith("#") && href.length > 1) {
+        document.querySelector(href).scrollIntoView({ behavior: "smooth" });
+      }
+
+      // disable mobile nav
+      if (el.classList.contains("nav-list-link")) {
+        bodyEl.classList.remove("mobile-nav-open");
+      }
+    });
+  });
+}
+
 updateCopyrightDate();
 enableMobileNavigation();
+enableSmoothScrolling();
