bc6167afd1f1832e6153c4cc0f17fc67e64019c8
Reinhard Liess <reinhard.liess@gmail.com>
2022-02-01 17:48:20 +0100

    feat: make mobile nav work

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,4 +6,14 @@ function updateCopyrightDate() {
   copyRightYearEl.textContent = new Date().getFullYear();
 }
 
+// make mobile navigation work
+function enableMobileNavigation() {
+  const bodyEl = document.querySelector("body");
+  const btnMobileNavEl = document.querySelector(".header--mobile-btn-nav");
+
+  btnMobileNavEl.addEventListener("click", function () {
+    bodyEl.classList.toggle("mobile-nav-open");
+  });
+}
 updateCopyrightDate();
+enableMobileNavigation();
