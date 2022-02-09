7ba81e0d522209673c4767317be27532f0aa68e7
Reinhard Liess <reinhard.liess@gmail.com>
2022-02-09 09:39:36 +0100

    fix: Fix flexbox gap for older browsers

--- resources/js/script.js
+++ resources/js/script.js
@@ -100,6 +100,26 @@ function enableStickyNavigation() {
   observer.observe(heroSectionEl);
 }
 
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log("Flexbox gap supported: ", isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+
+checkFlexGap();
+
 updateCopyrightDate();
 enableMobileNavigation();
 enableSmoothScrolling();
